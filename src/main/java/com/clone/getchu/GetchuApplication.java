62fb2bfc724644a683c75b08215bbbf7bec2634d
hyeonmin02 <hyenmin5@gmail.com>
2026-04-13 13:49:41 +0900

    Initial commit

---
+++ src/main/java/com/clone/getchu/GetchuApplication.java
@@ -0,0 +1,12 @@
+package com.clone.getchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetchuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetchuApplication.class, args);
+    }
+}
