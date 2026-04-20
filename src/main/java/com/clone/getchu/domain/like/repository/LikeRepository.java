99e9df56a18cd5302a0082d294e9e08d1e2a5a35
sosugyeong <sugyeong247@gmail.com>
2026-04-20 18:45:23 +0900

    feat: Like entity, repository 작성

--- src/main/java/com/clone/getchu/domain/like/repository/LikeRepository.java
+++ src/main/java/com/clone/getchu/domain/like/repository/LikeRepository.java
@@ -1,4 +1,7 @@
 package com.clone.getchu.domain.like.repository;
 
-public interface LikeRepository {
+import com.clone.getchu.domain.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
 }
