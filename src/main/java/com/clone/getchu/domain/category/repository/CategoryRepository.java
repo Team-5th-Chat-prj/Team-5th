71e9c254b36c95714f570e4eca064c56778e6e49
fryzke <fryekdy@gmail.com>
2026-04-20 16:30:06 +0900

    fix: 임포트 오류 수정

--- src/main/java/com/clone/getchu/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/clone/getchu/domain/category/repository/CategoryRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("SELECT new com.clone.getchu.domain.category.dto.response.CategoryResponse(c.id, c.name) " +
+    @Query("SELECT new com.clone.getchu.domain.category.dto.CategoryResponse(c.id, c.name) " +
             "FROM Category c")
     List<CategoryResponse> findAllProjections();
 }
